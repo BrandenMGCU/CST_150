e4b983b2d14d50a331c69ba4045ad493aa16d465
Branden M <brandenmcneill21@gmail.com>
2022-05-12 22:02:36 -0700

    Activity 8 CST-150
    Calorie Conversion Application.

---
+++ Activity 8 CST-150/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Activity_8_CST_150
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FatCalories = new System.Windows.Forms.TextBox();
+            this.CarbCalories = new System.Windows.Forms.TextBox();
+            this.CalorieTotal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(552, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Please enter your fat calorie count:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(581, 39);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Please enter your carb calorie count:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 310);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(573, 42);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Your total consumed calories are:";
+            // 
+            // FatCalories
+            // 
+            this.FatCalories.Location = new System.Drawing.Point(627, 77);
+            this.FatCalories.Multiline = true;
+            this.FatCalories.Name = "FatCalories";
+            this.FatCalories.Size = new System.Drawing.Size(263, 37);
+            this.FatCalories.TabIndex = 7;
+            this.FatCalories.TextChanged += new System.EventHandler(this.FatCalories_TextChanged);
+            // 
+            // CarbCalories
+            // 
+            this.CarbCalories.Location = new System.Drawing.Point(627, 203);
+            this.CarbCalories.Multiline = true;
+            this.CarbCalories.Name = "CarbCalories";
+            this.CarbCalories.Size = new System.Drawing.Size(263, 37);
+            this.CarbCalories.TabIndex = 8;
+            this.CarbCalories.TextChanged += new System.EventHandler(this.CarbCalories_TextChanged);
+            // 
+            // CalorieTotal
+            // 
+            this.CalorieTotal.BackColor = System.Drawing.SystemColors.Window;
+            this.CalorieTotal.Location = new System.Drawing.Point(627, 310);
+            this.CalorieTotal.Multiline = true;
+            this.CalorieTotal.Name = "CalorieTotal";
+            this.CalorieTotal.ReadOnly = true;
+            this.CalorieTotal.Size = new System.Drawing.Size(263, 193);
+            this.CalorieTotal.TabIndex = 9;
+            this.CalorieTotal.TextChanged += new System.EventHandler(this.CalorieTotal_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(158, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(352, 87);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calculate my Calories!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(907, 530);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CalorieTotal);
+            this.Controls.Add(this.CarbCalories);
+            this.Controls.Add(this.FatCalories);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calorie Convertor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FatCalories;
+        private System.Windows.Forms.TextBox CarbCalories;
+        private System.Windows.Forms.TextBox CalorieTotal;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Activity 8 CST-150/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Branden M.
+ * 05/12/22
+ * CST-150
+ * Prof. Mark Smithers
+ * Grand Canyon University
+ */
+
+namespace Activity_8_CST_150
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Welcome to the calorie counter app!" + "\n" + Environment.NewLine + "Simply input the amount of fat calorites and carb calories to get your result!", "Calorie Counter App");
+
+            int fat_count;
+            int fat_total;
+        }
+
+        private async void FatCalories_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // This will get the amount for the fat calorie count.
+                int fat_count = int.Parse(FatCalories.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("You must put in a number!","ERROR!");
+                FatCalories.Clear();
+                return;
+            }
+        }
+
+        private void CarbCalories_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // This will keep track of the calorie count for carbs.
+                int calorie_count = int.Parse(CarbCalories.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("You must put in a number!", "ERROR!");
+                CarbCalories.Clear();
+                return;
+            }
+        }
+
+        private void CalorieTotal_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // We will finalize the carb count and the fat calorie count.
+            int carb_total = int.Parse(CarbCalories.Text) * 4;
+            int fat_total = int.Parse(FatCalories.Text) * 9;
+
+            // This will then print out the results to the final textbox.
+            CalorieTotal.Text = "Your total amount of fat calories is: " + fat_total + "." + Environment.NewLine + Environment.NewLine
+                + '\n' + "Your total amount of carb calories is: " + carb_total + ".";
+        }
+    }
+}
