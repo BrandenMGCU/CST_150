f1f1ff90e8a52e089376f215cf22c1f029506e5b
Branden M <brandenmcneill21@gmail.com>
2022-05-20 19:36:46 -0700

    Activity 5 CST-150

---
+++ Activity 5 CST-150/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Activity_5_CST_150
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(182, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(839, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a file and upload it for input";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 533);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(402, 101);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Upload File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 421);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 69);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Alphabetize";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(390, 421);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(206, 69);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Lower Case";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(608, 421);
+            this.button4.Margin = new System.Windows.Forms.Padding(6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(192, 69);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Longest Word";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(812, 421);
+            this.button5.Margin = new System.Windows.Forms.Padding(6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(210, 69);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Most Vowels";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(608, 533);
+            this.button7.Margin = new System.Windows.Forms.Padding(6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(414, 101);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Save to File";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(194, 83);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(828, 329);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1216, 725);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Activity 5 (Text Handler)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Activity 5 CST-150/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/* Branden M.
+ * CST-150
+ * Prof. Smithers
+ * Grand Canyon University
+ * 05/07/22 */
+
+namespace Activity_5_CST_150
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Play a sound when the application begins.
+            System.Media.SoundPlayer intro_sound = new System.Media.SoundPlayer(@"C:\Users\Branden\source\repos\Activity 5 CST-150\Sounds\MagicWand.wav");
+            intro_sound.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // This is the foundation to opening a file.
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog.FileName);
+                richTextBox1.Text = sr.ReadToEnd();
+                
+
+                List<string> lines = System.IO.File.ReadAllLines(openFileDialog.FileName).ToList();
+                sr.Close();
+            }
+        }
+
+        private void openFileDialog1_FileOk_1(object sender, CancelEventArgs e)
+        {
+            // This is the open file dialog.
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // This will save the information in the box to a text file.
+            SaveFileDialog save_info = new SaveFileDialog();
+
+            // Show the save file screen and the format that it will be saved as.
+            save_info.Filter = "Text File (*.txt)|*.txt";
+            save_info.DefaultExt = "txt";
+
+
+
+
+            // Shows where the file will be saved at.
+            save_info.InitialDirectory = @"C:\Users\Branden\Documents";
+
+            // This will save the file to the directory.
+            if (save_info.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(save_info.FileName, richTextBox1.Text);
+            }
+
+
+            //Shows that the file has been saved and where the file is located.
+            MessageBox.Show("The file was saved and is located in: " + save_info.InitialDirectory, "File Saved Successfully!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // This will alphabetically sort the list of information.
+            string[] rtb = richTextBox1.Text.ToString().Split(' ');
+            IEnumerable<string> query = from word in rtb
+                                        orderby word.Substring(0, 1) ascending
+                                        select word;
+            richTextBox1.Clear();
+            foreach (string word in query)
+            {
+               richTextBox1.AppendText("" + word );
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // This will make the text in the list box lower case.
+           richTextBox1.Text = richTextBox1.Text.ToLower();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        // This will find the longest word inside of the box.
+        {
+            // String array using the RTB for the text.
+            string array = richTextBox1.Text;
+
+            // How we will seperate each word
+            string[] words = array.Split(new[] {" "}, StringSplitOptions.None);
+
+            //  This will implement the longest word counter.
+            string longest_word = "";
+            int word_counter = 0;
+
+
+            foreach (string s in words)
+            {
+                if (s.Length > word_counter)
+                {
+                    word_counter = s.Length;
+                    longest_word = s;
+                }
+            }
+            MessageBox.Show("The longest word is: " + longest_word.ToString(), "Longest Word");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // This will tell and show the word with the most vowels.
+            var input = richTextBox1.Text;
+
+            // The vowels we are looking for.
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            // Here are the characters we don't want to include.
+            char[] dont_want = { ' ', '.', ',', '!', '?', ':', ';', '(', ')'};
+
+            // The foundation for the word we need.
+            string word = "";
+
+            // This will further specify how we will split the words and we will remove any empty space.
+            string[] splitter_foundation = input.Split(dont_want, StringSplitOptions.RemoveEmptyEntries);
+
+            // Vowel counter being initalized at 0.
+            var mostVowels = 0;
+
+
+            for (int i = 0; i < splitter_foundation.Length; i++)
+            {
+                var part = splitter_foundation[i];
+
+                // Counter for the number of vowels inside of the word.
+                var numVowels = 0;
+                foreach (var vowel in vowels)
+                {
+                    if (part.Contains(vowel)) numVowels++;
+                }
+
+                if (mostVowels < numVowels)
+                {
+                    mostVowels = i;
+                    word = part;
+                }
+            }
+            MessageBox.Show("The word with the most vowels in it is: " + word.ToString(), "Most Vowels");
+            return;
+        }
+    }
+}
