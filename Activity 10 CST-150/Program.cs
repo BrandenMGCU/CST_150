5079d4aa3031b6f123edfe0695ee250df03bcd98
Branden M <brandenmcneill21@gmail.com>
2022-05-31 23:37:11 -0700

    Activity 10 CST-150

---
+++ Activity 10 CST-150/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Branden M
+ * 05.30.22
+ * Activity 10
+ * CST-150 
+ * Prof. Mark Smithers
+ * Grand Canyon University
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Corrected by Branden M. */
+
+/* Namespace needs to be changed to function with this project */
+
+// Original Code - namespace CST117_IC08_console
+
+namespace Activity_10_CST_150
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //make some sets
+            /* No Changes */
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            /* No Changes.*/ 
+            Random r = new Random();
+
+            /* No Changes. */
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+
+            /* Adding a line for a bit more context and clarification */
+            Console.WriteLine("Here are the given sets:");
+
+            Console.WriteLine("A: " + A);
+
+            Console.WriteLine("B: " + B);
+
+
+            /* Error, A.union(B) shows just Set A.
+             * 
+             * Original Code - Console.WriteLine("A union B: " + A.union(B));
+             */
+            
+            Console.WriteLine("A union B: " + A.union(A,B));
+
+            /* Adding a space for context and clarity */
+            Console.WriteLine();
+
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+
+        }
+    }
+}
+
---
+++ Activity 10 CST-150/Set.cs
@@ -0,0 +1,91 @@
+﻿/* Branden M
+ * 05.30.22
+ * Activity 10
+ * CST-150 
+ * Prof. Mark Smithers
+ * Grand Canyon University
+ */
+
+using System;
+
+/* Corrected by Branden M. */
+
+namespace Activity_10_CST_150
+{
+    public class Set
+{
+        private List<int> elements;
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i])
+                    return true;
+                /* Instead of else return false, return false was sufficent. */
+                 // Original Code - else return false;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        /* Add both Set A and B to keep it simple. 
+         * Create a unified Set to then add both Sets into the elements.
+         Change rhs to be both Set A and B now.*/
+
+
+        /*The original problem was:
+         * public Set union(Set rhs) { 
+            for (int i = 0; i < rhs.elements.Count; i++) {
+             this.addElement(rhs.elements[i]);
+            }
+            return rhs;
+        }*/
+
+        /* Restructured union function. */
+        public Set union(Set A, Set B)
+        {
+            Set unionSet = new Set();
+            for (int i = 0; i < A.elements.Count; i++)
+            {
+                unionSet.addElement(A.elements[i]);
+            }
+
+            for (int i = 0; i < B.elements.Count; i++)
+            {
+                unionSet.addElement(B.elements[i]);
+            }
+            return unionSet;
+        }
+
+    }
+}
