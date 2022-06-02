cab4df45e2fff04cc1c7b2e1df3ba3866c8d8665
Branden M <brandenmcneill21@gmail.com>
2022-06-02 00:16:51 -0700

    Activity 9 CST-150

---
+++ Activity 9 CST-150/Program.cs
@@ -0,0 +1,101 @@
+﻿/* Branden M.
+ * 06.01.22
+ * Activity 9
+ * CST-150
+ * Grand Canyon University */
+
+namespace Activity_9_CST_150
+{
+    /* The class will be Activity 9 */
+    public class Activity9
+    {
+        /* Here is the main method */
+        public static void Main(string[] args)
+        {
+            /* Put the Main method by itself because the error list
+             * was saying that some of the methods that were created
+             * but were never used. I didn't want to have any issues.
+             * Also it wouldn't allow me to make any of my methods
+             * public or private if needed. I made them private because,
+             * there is no need for them to be accessable outside of each other.
+             * Also, these are permanent.
+             */
+        }
+
+             private static void Two_Int_Sum(int num1, int num2)
+            {
+                // Grabing the two numbers total. 
+                return;
+            }
+            /* Problem 1, method 1, the sum of two integers */
+
+            private static void Five_Num_Double(double num1, double num2, double num3, double num4, double num5)
+            {
+                /* Grab the 5 numbers doubles and from their 
+                 * returning their sum. */
+                return;
+            }
+            /* Problem 2, Method 2, 5 Doubles and return the sum */
+
+            private static void Sum_of_Two_RandomNumbers(int r1, int r2)
+            {
+                // Adding the two random numbers and then I would return their sum.
+                return;
+            }
+            /* Problem 3, Method 3, 2 random numbers and return the sum */
+
+            private static bool Three_IntSum_divisible(int num1, int num2, int num3)
+            {
+                /* I would grab the sum of all three numbers and 
+                 * then divide the total by three and ensure that it
+                 * equals out to 0 and return that either it is
+                 * true or false */
+                return true;
+            }
+            /* Problem 4, Method 4, 3 numbers divisible by 3 */
+
+
+            private static string smallerString (string text1, string text2)
+            {
+                return "smallest string";
+            }
+            /* Problem 5, Method 5, finding the smallest string */
+
+            private double BiggestDoubleInside_Array(double[] intSampleArray)
+            {
+                /* Finding the biggest double inside the Array. */
+                return 0;
+            }
+            /* Problem 6, Method 6, find the largest double in the Array. */
+
+             private int[] createdIntegerArray()
+            {
+                int[] newArray = new int[50];
+                return newArray;
+            }
+            /* Problem 7, Method 7, Array of 50 integer values. */
+
+            private bool BoolVaribleTwins (bool option1, bool option2)
+            {
+                bool result = true;
+                return result;
+            }
+            /* Problem 8, Method 8, Two Bool variable matches or not. */
+
+            private double Int_Times_Double(int num1, double num2)
+            {
+                num1 = 0;
+                double result = 0;
+                return result * num1;
+            }
+            /* Problem 9, Method 9, Multiple an int and double for the result. */
+
+             private double twoDArrayAverage (int[,] array)
+            {
+                return 0;
+            }
+            /* Problem 10, Method 10, The average of a 2D array. */
+
+    }
+
+}
